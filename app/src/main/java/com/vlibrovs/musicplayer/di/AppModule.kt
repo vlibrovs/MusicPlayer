33237bab77960dccdb578c09f5b3d208c2893622
vlibrovs <vlad.librovsky@gmail.com>
2022-11-25 20:21:51 +0300

    Add FirebaseMusicSource.kt

--- app/src/main/java/com/vlibrovs/musicplayer/di/AppModule.kt
+++ app/src/main/java/com/vlibrovs/musicplayer/di/AppModule.kt
@@ -2,10 +2,8 @@ package com.vlibrovs.musicplayer.di
 
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.upstream.DefaultDataSource
-import com.vlibrovs.musicplayer.presentation.service.MusicService
 import com.vlibrovs.musicplayer.presentation.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
