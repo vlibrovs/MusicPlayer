d354420d5f60731e11fcba9deb85f4b6fc0aaa54
vlibrovs <vlad.librovsky@gmail.com>
2022-11-25 16:50:15 +0300

    MusicService.kt basic setup

--- app/src/main/java/com/vlibrovs/musicplayer/di/AppModule.kt
+++ app/src/main/java/com/vlibrovs/musicplayer/di/AppModule.kt
@@ -1,5 +1,11 @@
 package com.vlibrovs.musicplayer.di
 
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.vlibrovs.musicplayer.presentation.service.MusicService
 import com.vlibrovs.musicplayer.presentation.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -7,5 +13,22 @@ import org.koin.dsl.module
 val appModule = module {
     viewModel {
         MainViewModel()
+    }
+    single {
+        AudioAttributes.Builder()
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
+    }
+
+    single {
+        ExoPlayer.Builder(get()).build().apply {
+            setAudioAttributes(get(), true)
+            setHandleAudioBecomingNoisy(true)
+        }
+    }
+
+    single {
+        DefaultDataSource.Factory(get())
     }
 }
---
+++ app/src/main/java/com/vlibrovs/musicplayer/presentation/service/MusicService.kt
@@ -0,0 +1,62 @@
+package com.vlibrovs.musicplayer.presentation.service
+
+import android.app.PendingIntent
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+
+private const val SERVICE_TAG = "MusicService"
+
+class MusicService(
+    private val dataSourceFactory: DefaultDataSource.Factory,
+    private val exoPlayer: ExoPlayer
+) : MediaBrowserServiceCompat() {
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
+    override fun onCreate() {
+        super.onCreate()
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, 0)
+        }
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
+            setSessionActivity(activityIntent)
+            isActive = true
+        }
+        sessionToken = mediaSession.sessionToken
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlayer(exoPlayer)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
+}
