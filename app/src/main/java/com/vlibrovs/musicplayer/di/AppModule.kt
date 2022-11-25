21c83b24ef9a01ecee3262a49022e8f399b123b4
vlibrovs <vlad.librovsky@gmail.com>
2022-11-25 20:50:40 +0300

    FirebaseMusicSource.kt additions #1

--- app/src/main/java/com/vlibrovs/musicplayer/di/AppModule.kt
+++ app/src/main/java/com/vlibrovs/musicplayer/di/AppModule.kt
@@ -4,6 +4,8 @@ import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.vlibrovs.musicplayer.data.remote.MusicDatabase
+import com.vlibrovs.musicplayer.presentation.music.FirebaseMusicSource
 import com.vlibrovs.musicplayer.presentation.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -28,5 +30,13 @@ val appModule = module {
 
     single {
         DefaultDataSource.Factory(get())
+    }
+
+    single {
+        MusicDatabase()
+    }
+
+    single {
+        FirebaseMusicSource(get())
     }
 }
--- app/src/main/java/com/vlibrovs/musicplayer/presentation/music/FirebaseMusicSource.kt
+++ app/src/main/java/com/vlibrovs/musicplayer/presentation/music/FirebaseMusicSource.kt
@@ -1,8 +1,28 @@
 package com.vlibrovs.musicplayer.presentation.music
 
+import android.media.MediaMetadata.*
+import android.media.browse.MediaBrowser.MediaItem.FLAG_PLAYABLE
+import android.provider.MediaStore.Audio.Media
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import androidx.core.net.toUri
+import coil.decode.DataSource
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.vlibrovs.musicplayer.data.remote.MusicDatabase
 import com.vlibrovs.musicplayer.presentation.music.State.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class FirebaseMusicSource {
+class FirebaseMusicSource(
+    private val musicDatabase: MusicDatabase
+) {
+
+    var songs = emptyList<MediaMetadataCompat>()
 
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
@@ -19,6 +39,46 @@ class FirebaseMusicSource {
                 field = value
             }
         }
+
+    suspend fun fetchMediaData() = withContext(Dispatchers.IO) {
+        state = STATE_INITIALIZING
+        val allSongs = musicDatabase.getAllSongs()
+        songs = allSongs.map { song ->
+            MediaMetadataCompat.Builder()
+                .putString(METADATA_KEY_ARTIST, song.artist)
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaId)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_SUBTITLE, song.artist)
+                .putString(METADATA_KEY_DISPLAY_ICON_URI, song.imageUrl)
+                .putString(METADATA_KEY_MEDIA_URI, song.songUrl)
+                .putString(METADATA_KEY_ALBUM_ART_URI, song.imageUrl)
+                .putString(METADATA_KEY_DISPLAY_DESCRIPTION, song.artist)
+                .build()
+        }
+        state = STATE_INITIALIZED
+    }
+
+    fun asMediaSource(dataSourceFactory: DefaultDataSource.Factory): ConcatenatingMediaSource {
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        songs.forEach { song ->
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(song.getString(METADATA_KEY_MEDIA_URI)))
+            concatenatingMediaSource.addMediaSource(mediaSource)
+        }
+        return concatenatingMediaSource
+    }
+
+    fun asMediaItems() = songs.map { song ->
+        val desc = MediaDescriptionCompat.Builder()
+            .setMediaUri(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            .setTitle(song.description.title)
+            .setSubtitle(song.description.subtitle)
+            .setMediaId(song.description.mediaId)
+            .setIconUri(song.description.iconUri)
+            .build()
+        MediaBrowserCompat.MediaItem(desc, FLAG_PLAYABLE)
+    }
 
     fun whenReady(action: (Boolean) -> Unit): Boolean {
         return if (state == STATE_CREATED || state == STATE_INITIALIZING) {
